c392e8604690c41f7017b38888de87537bb6d394
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-28 16:40:07 +0530

    modal updated

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,9 +1,34 @@
 import React from "react";
-import classses from "./Modal.module.css";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
 
+const Backdrop = props => {
+    return(
+        <div className={classes.backdrop}>
 
+        </div>
+    )
+}
+
+const ModalOverlay = props => {
+    return(
+        <div className={classes.modal}>
+            <div className={classes.content}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+const fetchPortalElement = document.querySelector("#overlays";
+}
 const Modal = props => {
-    return null;
+    return(
+        <react.Fragment>
+            {ReactDOM.createPortal(<Backdrop />, fetchPortalElement)}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, fetchPortalElement)}
+        </react.Fragment>
+    )
 }
 
 export default Modal;
