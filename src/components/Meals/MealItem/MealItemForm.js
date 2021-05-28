1b71b51733ded1d0b6fbddaef15f6bfc9631048d
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-28 14:59:34 +0530

    right of the form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,21 @@
 import React from "react";
 import classes from "./MealItemForm.module.css"
+import Input from "../../UI/Input";
+
+
 const MealItemForm = () => {
     return(
         <form className={classes.form}>
-            <input />
+            <Input 
+                label="Amount" 
+                input={{
+                    id: "amount",
+                    type: "number",
+                    min: "1",
+                    max: "10",
+                    step: "1",
+                    defaultValue: "1"
+                }}/>
             <button>+ Add</button>
         </form>
     );
