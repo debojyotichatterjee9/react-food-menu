c754abb78e2b84d4a7847c3ffaf6f3bcbe008cab
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-28 23:01:03 +0530

    logic for repeating items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
 
@@ -7,17 +7,30 @@ const initialCartState = {
     totalAmount: 0
 }
 const cartReducer = (state = initialCartState, action) => {
-    if(action.type === "ADD_ITEM") {
+    if (action.type === "ADD_ITEM") {
         const newAddedItem = action.item;
-        const updatedItems = state.items.concat(newAddedItem);
         const updatedTotalAmount = state.totalAmount + (newAddedItem.price * newAddedItem.quantity);
-        console.log(updatedItems);
-        return({
+        const existingItemIndex = state.items.findIndex(item => item.id === newAddedItem.id)
+        const existingCartItem = state.items[existingItemIndex];
+        let updatedItems;
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity + newAddedItem.quantity
+            };
+console.log(updatedItem)
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem
+        }
+        else {
+            updatedItems = state.items.concat(newAddedItem)
+        }
+        return ({
             items: updatedItems,
             totalAmount: updatedTotalAmount
         })
     }
-    if(action.type === "REMOVE_ITEM") {
+    if (action.type === "REMOVE_ITEM") {
 
     }
     return initialCartState;
@@ -46,7 +59,7 @@ const CartProvider = props => {
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }
-    return(
+    return (
         <CartContext.Provider value={cartContext}>
             {props.children}
         </CartContext.Provider>
